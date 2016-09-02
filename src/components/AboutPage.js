cfbe9225564e1495d09bcd539e8c6a01f1d4e891
Andrew Vye <a.vye26@gmail.com>
2016-09-03 05:13:59 +0900

    (feat) Add Victory to visually display skills

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -2,14 +2,31 @@
 
 import React from 'react';
 import { Link } from 'react-router';
+import { VictoryBar } from 'victory';
+import skillsData from '../data/skillsData';
 
 export default class AboutPage extends React.Component {
   render() {
+    const axisValues = [
+      'JavaScript',
+      'React'
+    ]
     return (
       <div className="container">
         <div className="row">
-          <div className="">
+          <div className="col-xs-12 col-sm-10 col-md-8 col-xs-offset-0 col-sm-offset-1 col-md-offset-2">
             <h2>Andrew Vye</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 col-sm-10 col-md-8 col-xs-offset-0 col-sm-offset-1 col-md-offset-2">
+            <VictoryBar
+              horizontal={true}
+              data={skillsData}
+            />
+          </div>
+          <div className="col-xs-12 col-sm-10 col-md-8 col-xs-offset-0 col-sm-offset-1 col-md-offset-2">
+
           </div>
         </div>
       </div>
---
+++ src/data/skillsData.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const skillsData = [
+  {
+    x: 1,
+    y: 1,
+    label: 'JavaScript'
+  },
+  {
+    x: 2,
+    y: 2,
+    label: 'React'
+  }
+];
+
+export default skillsData;
