9410ab2b3b5eff01787f54cc2a14b2ffeb17f327
Andrew Vye <a.vye26@gmail.com>
2016-09-03 05:57:16 +0900

    (feat) Add in Victory pie charts to visually display skills

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { Link } from 'react-router';
-import { VictoryBar } from 'victory';
+import { VictoryPie } from 'victory';
 import skillsData from '../data/skillsData';
 
 export default class AboutPage extends React.Component {
@@ -14,16 +14,34 @@ export default class AboutPage extends React.Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-xs-12 col-sm-10 col-md-8 col-xs-offset-0 col-sm-offset-1 col-md-offset-2">
+          <div className="col-xs-12 col-sm-10 col-md-8 col-xs-offset-0 col-sm-offset-1 col-md-offset-2 text-center">
             <h2>Andrew Vye</h2>
           </div>
         </div>
         <div className="row">
-          <div className="col-xs-12 col-sm-10 col-md-8 col-xs-offset-0 col-sm-offset-1 col-md-offset-2">
-            <VictoryBar
-              horizontal={true}
-              data={skillsData}
+          <div className="col-xs-12 col-sm-12 col-md-4 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 text-center">
+            <h4>Front-End</h4>
+            <VictoryPie
+              padAngle={5}
+              data={skillsData.frontend}
             />
+            <p>TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT </p>
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-4 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 text-center">
+            <h4>Front-End</h4>
+            <VictoryPie
+              padAngle={5}
+              data={skillsData.frontend}
+            />
+            <p>TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT </p>
+          </div>
+          <div className="col-xs-12 col-sm-12 col-md-4 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 text-center">
+            <h4>Front-End</h4>
+            <VictoryPie
+              padAngle={5}
+              data={skillsData.frontend}
+            />
+            <p>TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT TONS OF TEXT </p>
           </div>
           <div className="col-xs-12 col-sm-10 col-md-8 col-xs-offset-0 col-sm-offset-1 col-md-offset-2">
 
--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,16 +1,210 @@
 "use strict";
 
-const skillsData = [
-  {
-    x: 1,
-    y: 1,
-    label: 'JavaScript'
-  },
-  {
-    x: 2,
-    y: 2,
-    label: 'React'
-  }
-];
+const skillsData = {
+  frontend: [
+    {
+      y: 1,
+      label: 'Bootstrap/CSS3'
+    },
+    {
+      y: 2,
+      label: 'jQuery'
+    },
+    {
+      y: 3,
+      label: 'Webpack/Grunt'
+    },
+    {
+      y: 4,
+      label: 'HTML5'
+    },
+    {
+      y: 5,
+      label: 'Test Driven Development'
+    },
+    {
+      y: 6,
+      label: 'Continuous Integration'
+    },
+    {
+      y: 7,
+      label: 'Git'
+    },
+    {
+      y: 8,
+      label: 'ES6'
+    },
+    {
+      y: 10,
+      label: 'Angular'
+    },
+    {
+      y: 12,
+      label: 'React Native (mobile)'
+    },
+    {
+      y: 13,
+      label: 'React/Redux'
+    },
+    {
+      y: 15,
+      label: 'JavaScript'
+    },
+  ],
+  backend: [
+    {
+      x: 1,
+      y: 1,
+      label: 'Bootstrap/CSS3'
+    },
+    {
+      x: 2,
+      y: 2,
+      label: 'jQuery'
+    },
+    {
+      x: 3,
+      y: 3,
+      label: 'Webpack/Grunt'
+    },
+    {
+      x: 4,
+      y: 4,
+      label: 'HTML5'
+    },
+    {
+      x: 5,
+      y: 3,
+      label: 'Test Driven Development'
+    },
+    {
+      x: 6,
+      y: 1,
+      label: 'Continuous Integration'
+    },
+    {
+      x: 7,
+      y: 2,
+      label: 'Git'
+    },
+    {
+      x: 8,
+      y: 3,
+      label: 'ES6'
+    },
+    {
+      x: 9,
+      y: 4,
+      label: 'PostgreSQL'
+    },
+    {
+      x: 10,
+      y: 3,
+      label: 'Angular'
+    },
+    {
+      x: 11,
+      y: 1,
+      label: 'MongoDB'
+    },
+    {
+      x: 12,
+      y: 2,
+      label: 'React Native (mobile)'
+    },
+    {
+      x: 13,
+      y: 3,
+      label: 'React/Redux'
+    },
+    {
+      x: 14,
+      y: 4,
+      label: 'Node/Express'
+    },
+    {
+      x: 15,
+      y: 3,
+      label: 'JavaScript'
+    },
+  ],
+  general: [
+    {
+      x: 1,
+      y: 1,
+      label: 'Bootstrap/CSS3'
+    },
+    {
+      x: 2,
+      y: 2,
+      label: 'jQuery'
+    },
+    {
+      x: 3,
+      y: 3,
+      label: 'Webpack/Grunt'
+    },
+    {
+      x: 4,
+      y: 4,
+      label: 'HTML5'
+    },
+    {
+      x: 5,
+      y: 3,
+      label: 'Test Driven Development'
+    },
+    {
+      x: 6,
+      y: 1,
+      label: 'Continuous Integration'
+    },
+    {
+      x: 7,
+      y: 2,
+      label: 'Git'
+    },
+    {
+      x: 8,
+      y: 3,
+      label: 'ES6'
+    },
+    {
+      x: 9,
+      y: 4,
+      label: 'PostgreSQL'
+    },
+    {
+      x: 10,
+      y: 3,
+      label: 'Angular'
+    },
+    {
+      x: 11,
+      y: 1,
+      label: 'MongoDB'
+    },
+    {
+      x: 12,
+      y: 2,
+      label: 'React Native (mobile)'
+    },
+    {
+      x: 13,
+      y: 3,
+      label: 'React/Redux'
+    },
+    {
+      x: 14,
+      y: 4,
+      label: 'Node/Express'
+    },
+    {
+      x: 15,
+      y: 3,
+      label: 'JavaScript'
+    },
+  ]
+}
 
 export default skillsData;
