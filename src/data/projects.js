ca0967c2a1f468268139279d96446c864d863e51
Andrew Vye <a.vye26@gmail.com>
2016-09-03 02:20:00 +0900

    (chore) Update pictures and picture references

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,10 +5,10 @@ const projects = [
     id: 'followthru',
     name: 'followthru',
     subtitle: 'UI, FRONT-END DESIGN, BACK-END ARCHITECTURE',
-    image1: 'savor1.png',
-    image2: 'savor2.png',
-    link: 'https://savor-app.herokuapp.com/#/',
-    button: 'View Site',
+    image1: 'followthru1.png',
+    image2: 'followthru2.png',
+    link: 'https://vimeo.com/173844724',
+    button: 'View Demo',
     description: 'Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. '
   },
   {
@@ -25,9 +25,9 @@ const projects = [
     id: 'trex-timer',
     name: 'Trex Timer',
     subtitle: 'UI, FRONT-END DESIGN, BACK-END ARCHITECTURE',
-    image1: 'savor1.png',
-    image2: 'savor2.png',
-    link: 'https://savor-app.herokuapp.com/#/',
+    image1: 'trex-racer1.png',
+    image2: 'trex-racer2.png',
+    link: 'https://trex-timer.herokuapp.com/#/signin',
     button: 'View Site',
     description: 'Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. Savor was an app I created to make cool stuff. '
   }
