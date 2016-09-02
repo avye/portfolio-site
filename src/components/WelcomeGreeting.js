15f04a258c2b5b3cd7b8e0e0411edc02f6631212
Andrew Vye <a.vye26@gmail.com>
2016-09-03 03:46:36 +0900

    (feat) Update sizing on Welcome Greeting to be more responsive

--- src/components/WelcomeGreeting.js
+++ src/components/WelcomeGreeting.js
@@ -22,12 +22,12 @@ export default class WelcomeGreeting extends React.Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-xs-4 col-md-4 col-md-offset-4 col-xs-offset-4 center-text">
+          <div className="col-xs-8 col-sm-6 col-md-4 col-xs-offset-2 col-sm-offset-3 col-md-offset-4  center-text">
             <img src="/img/me.jpg" className="img-responsive img-circle center-block"/>
           </div>
         </div>
         <div className="row">
-          <div className="col-xs-6 col-md-6 col-md-offset-3 col-xs-offset-3 center-text">
+          <div className="col-xs-12 col-sm-10 col-md-6 col-xs-offset-0 col-sm-offset-1 col-md-offset-3 center-text">
             <h1 className="text-center"><strong>Welcome!</strong></h1>
             <h4 className="text-center">I'm a full stack software engineer.</h4>
             <h4 className="text-center">I enjoy playing in both mobile and web environments.</h4>
