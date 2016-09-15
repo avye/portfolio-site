e410a891a3754650251453f9bc3b3449f6832269
Andrew Vye <a.vye26@gmail.com>
2016-09-15 12:34:30 +0900

    (feat) Update about page skill content

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -14,7 +14,7 @@ export default class AboutPage extends React.Component {
       <div className="container aboutPageContainer">
         <div className="row">
           <div className="col-xs-12 col-sm-10 col-md-8 col-xs-offset-0 col-sm-offset-1 col-md-offset-2 text-center">
-            <h1>I can build whatever you want.</h1>
+            <h1 className="text-center"><big><strong>Hello.</strong></big></h1>
             <h4>I have hiked the Pacific Crest Trail, I have traveled throughout the world, and I have found what I love doing.  I hope to share that with you.</h4>
           </div>
         </div>
@@ -85,7 +85,7 @@ export default class AboutPage extends React.Component {
         </div>
         <div className="row">
           <div className="col-xs-12 col-sm-10 col-md-8 col-xs-offset-0 col-sm-offset-1 col-md-offset-2 text-center">
-            <h4>Always staying current with industry trends, I have worked with many of the popular front-end MVC frameworks such as React and Angular to create highly functional applications. Additionally I have developed both RESTful applications as well as ones that utilize Websockets. Whether it is utilizing popular APIs such as Yelp to create an immersive experience, or just wanting a well designed site, I can handle it.</h4>
+            <h4>I have worked with many of the popular front-end MVC frameworks such as React and Angular to create highly functional applications. Combining back-end tools such as Node.js/Express and databases such as MongoDB/SQL I can create a full-stack web application. Whether it is a responsive Single Page App or even an iOS/Android mobile application, you can know that I will do everything to ensure your vision is wonderfully created.</h4>
           </div>
         </div>
         <div className="row">
@@ -101,28 +101,6 @@ export default class AboutPage extends React.Component {
             </Link>
           </div>
         </div>
-
-          {/*<div className="col-xs-12 col-sm-12 col-md-4 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 text-center">
-            <h4><strong>Front-End Development</strong></h4>
-            <VictoryPie
-              data={skillsData.frontend}
-            />
-            <p>Always staying current with industry trends, I have worked with many of the popular front-end MVC frameworks such as React and Angular to create highly functional applications. Additionally I have developed both RESTful applications as well as ones that utilize Websockets. Whether it is utilizing popular APIs such as Yelp to create an immersive experience, or just wanting a well designed site, I can handle it.</p>
-          </div>
-          <div className="col-xs-12 col-sm-12 col-md-4 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 text-center">
-            <h4><strong>Full Stack Development</strong></h4>
-            <VictoryPie
-              data={skillsData.backend}
-            />
-            <p>Combining back-end tools such as Node.js/Express and databases such as MongoDB/SQL along with front-end frameworks such as React or Angular I can create a full-stack web application. Whether it is a responsive Single Page App or even an iOS/Android mobile application, you can know that I will do everything to ensure your vision is wonderfully created.</p>
-          </div>
-          <div className="col-xs-12 col-sm-12 col-md-4 col-xs-offset-0 col-sm-offset-0 col-md-offset-0 text-center center-block">
-            <h4><strong>Best Practices</strong></h4>
-            <VictoryPie
-              data={skillsData.general}
-            />
-            <p>Every application is different and I take the time to fully understand your needs. From there, I can utilize industry best practices such as Test Driven Development and Continuous Integration to ensure that you have a robust product that leaves your users satisfied.</p>
-          </div>*/}
       </div>
     );
   }
