12cc765d8c4bd2f52f0a2f66f77eaabfa694a827
Andrew Vye <a.vye26@gmail.com>
2016-09-16 09:45:39 +0900

    (feat) Add Medium to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,10 +21,10 @@ export default class Footer extends React.Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-xs-5 col-sm-4 col-md-2 col-xs-offset-0 col-sm-offset-0 col-md-offset-0">
+          <div className="col-xs-4 col-sm-4 col-md-2 col-xs-offset-0 col-sm-offset-0 col-md-offset-0">
             <p>Copyright {new Date().getFullYear()} &copy;</p>
           </div>
-          <div className="col-xs-2 col-sm-1 col-md-1 col-xs-offset-1 col-sm-offset-5 col-md-offset-7">
+          <div className="col-xs-2 col-sm-1 col-md-1 col-xs-offset-0 col-sm-offset-4 col-md-offset-6">
             <a href="https://www.linkedin.com/in/andrewvye">
               <img src="/img/LinkedIn.svg" className="img-responsive center-block" />
             </a>
@@ -38,6 +38,11 @@ export default class Footer extends React.Component {
             <Link to="contact">
               <img src="/img/SpeechBubble.svg" className="img-responsive center-block" />
             </Link>
+          </div>
+          <div className="col-xs-2 col-sm-1 col-md-1">
+            <a href="https://medium.com/@a.vye">
+              <img src="/img/MediumLogo.svg" className="img-responsive center-block" />
+            </a>
           </div>
         </div>
       </div>
