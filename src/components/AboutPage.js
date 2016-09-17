5b1f50a9088a6cdb8c6e8c2c6d9ad047d99adaa7
Andrew Vye <a.vye26@gmail.com>
2016-09-17 10:31:36 +0900

    (feat) Add in query to determine if user on tablet/phone and then utilize two charts for clarity/ease of use

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -3,8 +3,9 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { VictoryChart, VictoryBar, VictoryAxis } from 'victory';
+import { applyContainerQuery } from 'react-container-query';
 
-export default class AboutPage extends React.Component {
+class AboutPage extends React.Component {
   render() {
     const axisValues = [
       'JavaScript',
@@ -20,24 +21,102 @@ export default class AboutPage extends React.Component {
         </div>
         <div className="row">
           <div className="col-xs-12 col-sm-12 col-md-12 col-xs-offset-0 col-sm-offset-0 col-md-offset-0">
-            <VictoryChart height={250}>
+            {this.props.containerQuery.widthLargerThan1000 ?
+              <VictoryChart
+                height={200}
+                padding={{
+                  top: 20,
+                  bottom: 20,
+                  left: 70,
+                  right: 30
+                }}
+              >
+                <VictoryAxis
+                  tickValues={[
+                    "Redux",
+                    "React",
+                    "Angular",
+                    "React Native",
+                    "Node",
+                    "PostgreSQL",
+                    "MongoDB",
+                    "MySQL",
+                    "Socket.IO",
+                    "UI/UX",
+                    "Webpack/Grunt",
+                  ]}
+                  style={{
+                    axis: {stroke: "transparent"},
+                    tickLabels: {
+                      fontSize: 7
+                    }
+                  }}
+                />
+                <VictoryAxis
+                  dependentAxis
+                  style={{
+                    axis: {stroke: "transparent"},
+                    ticks: {stroke: "transparent"},
+                    tickLabels: {
+                      fontSize: 7
+                    }
+                  }}
+                  tickValues={[
+                    "Beginner",
+                    "Intermediate",
+                    "Advanced",
+                    "Expert"
+                  ]}
+                />
+                <VictoryBar
+                  data={[
+                    {x: 1, y: 3},
+                    {x: 2, y: 4},
+                    {x: 3, y: 3},
+                    {x: 4, y: 3},
+                    {x: 5, y: 4},
+                    {x: 6, y: 2},
+                    {x: 7, y: 3},
+                    {x: 8, y: 2},
+                    {x: 9, y: 2},
+                    {x: 10, y: 3},
+                    {x: 11, y: 2}
+                  ]}
+                  style={{
+                    data: {
+                    fill: (data) => data.y === 4 ?
+                      "rgb(225,206,148)" : data.y === 3 ?
+                      "rgb(202,185,133)" : data.y === 2 ?
+                      "rgb(180,164,118)" : "rgb(157,144,103)"
+                    }
+                  }}
+                  />
+              </VictoryChart>
+
+            :
+
+            <div>
+            <VictoryChart
+              height={250}
+              padding={{
+                top: 20,
+                bottom: 30,
+                left: 75,
+                right: 40
+              }}
+            >
               <VictoryAxis
                 tickValues={[
                   "Redux",
                   "React",
                   "Angular",
                   "React Native",
-                  "Node",
-                  "PostgreSQL",
-                  "MongoDB",
-                  "MySQL",
-                  "Socket.IO",
-                  "UI/UX",
-                  "Webpack/Grunt",
+                  "Node"
                 ]}
                 style={{
+                  axis: {stroke: "transparent"},
                   tickLabels: {
-                    fontSize: 6
+                    fontSize: 12
                   }
                 }}
               />
@@ -47,7 +126,7 @@ export default class AboutPage extends React.Component {
                   axis: {stroke: "transparent"},
                   ticks: {stroke: "transparent"},
                   tickLabels: {
-                    fontSize: 6
+                    fontSize: 12
                   }
                 }}
                 tickValues={[
@@ -63,24 +142,81 @@ export default class AboutPage extends React.Component {
                   {x: 2, y: 4},
                   {x: 3, y: 3},
                   {x: 4, y: 3},
-                  {x: 5, y: 4},
-                  {x: 6, y: 2},
-                  {x: 7, y: 3},
-                  {x: 8, y: 2},
-                  {x: 9, y: 2},
-                  {x: 10, y: 3},
-                  {x: 11, y: 2}
+                  {x: 5, y: 4}
                 ]}
                 style={{
                   data: {
                   fill: (data) => data.y === 4 ?
-                    "rgb(36,204,173)" : data.y === 3 ?
-                    "rgb(28,163,138)" : data.y === 2 ?
-                    "rgb(21,122,103)" : "rgb(14,81,69)"
+                    "rgb(225,206,148)" : data.y === 3 ?
+                    "rgb(202,185,133)" : data.y === 2 ?
+                    "rgb(180,164,118)" : "rgb(157,144,103)"
                   }
                 }}
                 />
             </VictoryChart>
+
+            <VictoryChart
+              height={250}
+              padding={{
+                top: 20,
+                bottom: 30,
+                left: 75,
+                right: 40
+              }}
+            >
+              <VictoryAxis
+                tickValues={[
+                  "PostgreSQL",
+                  "MongoDB",
+                  "MySQL",
+                  "Socket.IO",
+                  "UI/UX",
+                  "Webpack/Grunt"
+                ]}
+                style={{
+                  axis: {stroke: "transparent"},
+                  tickLabels: {
+                    fontSize: 12
+                  }
+                }}
+              />
+              <VictoryAxis
+                dependentAxis
+                style={{
+                  axis: {stroke: "transparent"},
+                  ticks: {stroke: "transparent"},
+                  tickLabels: {
+                    fontSize: 12
+                  }
+                }}
+                tickValues={[
+                  "Beginner",
+                  "Intermediate",
+                  "Advanced",
+                  "Expert"
+                ]}
+              />
+              <VictoryBar
+                data={[
+                  {x: 1, y: 2},
+                  {x: 2, y: 3},
+                  {x: 3, y: 2},
+                  {x: 4, y: 2},
+                  {x: 5, y: 3},
+                  {x: 6, y: 2}
+                ]}
+                style={{
+                  data: {
+                  fill: (data) => data.y === 4 ?
+                    "rgb(225,206,148)" : data.y === 3 ?
+                    "rgb(202,185,133)" : data.y === 2 ?
+                    "rgb(180,164,118)" : "rgb(157,144,103)"
+                  }
+                }}
+                />
+            </VictoryChart>
+            </div>
+            }
           </div>
         </div>
         <div className="row">
@@ -105,3 +241,12 @@ export default class AboutPage extends React.Component {
     );
   }
 }
+
+const query = {
+  'widthLargerThan1000': {
+    minWidth: 1000,
+  }
+};
+
+const AboutPageComponent = applyContainerQuery(AboutPage, query);
+export default AboutPageComponent;
